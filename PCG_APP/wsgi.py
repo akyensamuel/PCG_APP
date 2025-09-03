58a447898b40ed94a4439f809e1416138db6e55b
Akyen Samuel <lordsades1@gmail.com>
2025-09-03 14:47:52 +0000

    Fix static files collection for Django admin styling in production

--- PCG_APP/wsgi.py
+++ PCG_APP/wsgi.py
@@ -8,12 +8,24 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PCG_APP.settings')
+
+# Collect static files on startup for production
+import django
+from django.conf import settings
+django.setup()
+
+if not settings.DEBUG:
+    try:
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+    except Exception:
+        pass  # Ignore errors during static collection
 
 application = get_wsgi_application()
 
 # Vercel serverless function entry point
 app = application
+handler = application
